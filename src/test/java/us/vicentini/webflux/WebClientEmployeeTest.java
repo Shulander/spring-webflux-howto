feb777692a90aec4a6b2484a8519f0b02173168e
Henrique Vicentini <Henrique_A_Vicentini@gap.com>
2021-11-23 12:02:19 -0800

    Work with Response Entity

--- src/test/java/us/vicentini/webflux/WebClientEmployeeTest.java
+++ src/test/java/us/vicentini/webflux/WebClientEmployeeTest.java
@@ -3,6 +3,7 @@ package us.vicentini.webflux;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
@@ -10,6 +11,13 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 import us.vicentini.webflux.entity.Employee;
 import us.vicentini.webflux.exception.EmployeeException;
+
+import java.net.HttpCookie;
+import java.util.Collection;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @WireMockTest(httpPort = 8081)
@@ -99,6 +107,46 @@ class WebClientEmployeeTest {
                 .expectErrorMatches(throwable -> throwable instanceof EmployeeException &&
                                                  throwable.getMessage().equals("Client Not Found Error"))
                 .verify();
+    }
+
+
+    @Test
+    void shouldGetResponseEntityWithSingleObject() {
+        // web client base configuration
+        var webClient = WebClient.builder()
+                .baseUrl("http://localhost:8081")
+                .build();
+
+        // get request
+        var entity = webClient
+                .get()
+                .uri("/employees/123")
+                .retrieve()
+                .toEntity(Employee.class)
+                .block();
+
+        //assertions
+        assertNotNull(entity);
+        var status = entity.getStatusCode();
+        var cookie = getCookie(entity.getHeaders(), "SESSION");
+        var employee = entity.getBody();
+        assertNotNull(status);
+        assertNotNull(cookie);
+        assertNotNull(employee);
+        assertEquals(HttpStatus.OK, status);
+        assertEquals("SESSION", cookie.getName());
+        assertEquals("GPUTWPm4dJTyHbrun3HSbm", cookie.getValue());
+        assertEquals(new Employee(123L, "John Doe"), employee);
+    }
+
+
+    private HttpCookie getCookie(HttpHeaders headers, String cookieName) {
+        return Objects.requireNonNull(headers.get(HttpHeaders.SET_COOKIE))
+                .stream()
+                .map(HttpCookie::parse)
+                .flatMap(Collection::stream)
+                .filter(httpCookie -> httpCookie.getName().equals(cookieName))
+                .findFirst().orElse(null);
     }
 
 }
